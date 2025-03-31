ce49d282516d3c1c0b3368621af1f732093955f0
Yoan Sebastian <yoansebastianramirezgarcia@gmail.com>
2025-03-30 22:54:19 -0500

    resolve bug admin variable

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -10,7 +10,7 @@ function inicializarProjectoFirebase() {
     if (!apps.length) {
         initializeApp({
             credential: cert({
-                projectId: process.env.FIREBASE_PROJECT_ID,
+                projectId: process.env.FIREBASE_PROJECT_ID,  //resolviendo bug de la varibale
                 privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
                 clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
             })
